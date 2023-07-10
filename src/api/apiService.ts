feb07301d59c9f6a1a33e3e96a04ffa27a335f6d
charith <charith9422@gmail.com>
2023-07-10 10:20:57 +0530

    fix: env configured

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,12 +1,10 @@
 import axios, { AxiosResponse } from "axios";
 
-const API_BASE_URL = "https://www.boredapi.com/api";
-
 // common method for all GET calls
 export const getAPI = async <T>(url: string): Promise<T> => {
 	try {
 		const response: AxiosResponse<T> = await axios.get(
-			`${API_BASE_URL}/${url}`
+			`${process.env.REACT_APP_API_URL}/${url}`
 		);
 		return response.data;
 	} catch (error) {
